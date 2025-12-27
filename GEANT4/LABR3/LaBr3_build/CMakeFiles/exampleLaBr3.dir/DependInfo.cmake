543dd02a18a41d5db5e8b8cb3786a93caf5de17a
Richardforphys <ubuntu@DESKTOP-6DBJOLB.localdomain>
2025-12-27 22:52:15 +0100

    Working on LaBr3 example

---
+++ GEANT4/LABR3/LaBr3_build/CMakeFiles/exampleLaBr3.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/LaBr3.cc" "CMakeFiles/exampleLaBr3.dir/LaBr3.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/LaBr3.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/DetectorConstruction.cc" "CMakeFiles/exampleLaBr3.dir/src/DetectorConstruction.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/DetectorConstruction.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/DetectorMessenger.cc" "CMakeFiles/exampleLaBr3.dir/src/DetectorMessenger.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/DetectorMessenger.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/EventAction.cc" "CMakeFiles/exampleLaBr3.dir/src/EventAction.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/EventAction.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/GasHit.cc" "CMakeFiles/exampleLaBr3.dir/src/GasHit.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/GasHit.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/PrimaryGeneratorAction.cc" "CMakeFiles/exampleLaBr3.dir/src/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/RootSaver.cc" "CMakeFiles/exampleLaBr3.dir/src/RootSaver.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/RootSaver.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/RunAction.cc" "CMakeFiles/exampleLaBr3.dir/src/RunAction.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/RunAction.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/SensitiveDetector.cc" "CMakeFiles/exampleLaBr3.dir/src/SensitiveDetector.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/SensitiveDetector.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/StackingAction.cc" "CMakeFiles/exampleLaBr3.dir/src/StackingAction.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/StackingAction.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/TrackParentParticle.cc" "CMakeFiles/exampleLaBr3.dir/src/TrackParentParticle.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/TrackParentParticle.cc.o.d"
+  "/home/ubuntu/SMRR/GEANT4/LABR3/LaBr3/src/TrackParticle.cc" "CMakeFiles/exampleLaBr3.dir/src/TrackParticle.cc.o" "gcc" "CMakeFiles/exampleLaBr3.dir/src/TrackParticle.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
