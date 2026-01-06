47e2edf0bee225301b123ac188be4b6dfa3324e9
Richardforphys <r.maifredi@campus.unimib.it>
2026-01-06 19:51:19 +0100

    .

--- GEANT4/LaBr3/include/DetectorConstruction.hh
+++ GEANT4/LaBr3/include/DetectorConstruction.hh
@@ -3,14 +3,14 @@
 
 #include "globals.hh"
 #include "G4VUserDetectorConstruction.hh" //classe virtuale da cui derivo il detector
-#include "G4ThreeVector.hh"              //per definire i vettori 3D
-#include "G4SystemOfUnits.hh"          //per usare le unità di misura di Geant4
+#include "G4ThreeVector.hh"               //per definire i vettori 3D
+#include "G4SystemOfUnits.hh"             //per usare le unità di misura di Geant4
 /*================================================== CLASSES ==================================================*/
 
 class G4LogicalVolume;
 class G4VPhysicalVolume;
 class G4Material; 
-class DetectorMessenger;// questa classe e le precedenti sono incluse all'interno di G4VUserDetectorConstruction.hh
+class DetectorMessenger; // questa classe e le precedenti sono incluse all'interno di G4VUserDetectorConstruction.hh
 
 /*Ora costruiamo la classe DetectorConstruction che serve per costruire il rilevatore*/
 class DetectorConstruction: public G4VUserDetectorConstruction //deve avere lo stesso nome del file, ":" vuol dire "derivata da" quello che segue
@@ -55,5 +55,4 @@ class DetectorConstruction: public G4VUserDetectorConstruction //deve avere lo s
         G4double halfLaBr3Det_z;
 
         DetectorMessenger* detectorMessenger; //puntatore al messenger del rilevatore
-
     };
