a828bfa96cddb9f5e767778a18d13e4f529c56ba
Richardforphys <r.maifredi@campus.unimib.it>
2026-01-26 18:08:25 +0100

    Trying to commit exercises

---
+++ Exercises/task4/task4d/show.C
@@ -0,0 +1,9 @@
+{
+  TFile* file = TFile::Open("run_0.root");
+  TCanvas* c =new TCanvas();
+  c->Divide(1,2);
+  c->cd(1);
+  file->Get("LongitudinalProfile")->Draw();
+  c->cd(2);
+  file->Get("TotalEnergy")->Draw();
+}
