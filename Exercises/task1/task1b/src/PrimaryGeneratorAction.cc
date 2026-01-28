158c8449880c604baa96173c9019b295e2f54791
Richardforphys <r.maifredi@campus.unimib.it>
2026-01-28 17:34:35 +0100

    Task1b is done!

--- Exercises/task1/task1b/src/PrimaryGeneratorAction.cc
+++ Exercises/task1/task1b/src/PrimaryGeneratorAction.cc
@@ -65,3 +65,7 @@ G4VPrimaryGenerator* PrimaryGeneratorAction::InitializeGPS()
   return gps;
 }
 
+/* La sorgente in questione è un fascio monoenergetico di pioni a 2GeV,
+centrato in (0,0,-80cm) con una divergenza angolare gaussiana lungo z 
+di 0.1 mrad sia in x che in y e con FWHM iniziale di 0.1 mm
+*/
