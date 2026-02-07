a8061313f09ed13d5c3815e7be879b083f2eaf14
Richardforphys <r.maifredi@campus.unimib.it>
2026-02-07 14:52:29 +0100

    Deleted task4e and task4d

--- Exercises/task4/task4d/show.C
+++
@@ -1,9 +0,0 @@
-{
-  TFile* file = TFile::Open("run_0.root");
-  TCanvas* c =new TCanvas();
-  c->Divide(1,2);
-  c->cd(1);
-  file->Get("LongitudinalProfile")->Draw();
-  c->cd(2);
-  file->Get("TotalEnergy")->Draw();
-}
