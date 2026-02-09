ee94103669a1ecc15d083a3b513c711e93d436db
Richardforphys <r.maifredi@campus.unimib.it>
2026-02-09 17:35:45 +0100

    Task1 done

--- Exercises/task1/task1b/src/DetectorConstruction.cc
+++ Exercises/task1/task1b/src/DetectorConstruction.cc
@@ -343,7 +343,8 @@ G4VPhysicalVolume* DetectorConstruction::ConstructHadCalo()
 
 	G4Colour green(0,1,0);
 	G4Colour white(1,1,1);
-	hadCaloLogic->SetVisAttributes(new G4VisAttributes(green));
-	hadLayerLogic->SetVisAttributes(new G4VisAttributes(white));
+	G4Colour brown(0.4,0.4,0.1);
+	hadCaloLogic->SetVisAttributes(new G4VisAttributes(brown));
+	hadLayerLogic->SetVisAttributes(new G4VisAttributes(brown));
 	return hadCalo;
 }
